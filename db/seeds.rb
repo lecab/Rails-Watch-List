30c6d41b327ac2abf17dd636fe91e6b4ab9f223c
Guillaume Cabaret <gcabaret.cab@gmail.com>
2021-11-12 16:54:55 +0100

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
-require 'json'
 require 'open-uri'
+require 'json'
 
+puts "Cleaning up database..."
 Movie.destroy_all
+puts "Database cleaned"
 
 url = "http://tmdb.lewagon.com/movie/top_rated"
-    movies = URI.open(url).read
-    movie = JSON.parse(movies)
-    puts movie['results'].each do |x|
-      Movie.new({
-         title: x['original_title'],
-         overview: x['overview'],
-         poster_url: "https://image.tmdb.org/t/p/w500#{x['poster_path']}",
-         rating: x['vote_average']
-      })
-    end
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
+end
+puts "Movies created"
